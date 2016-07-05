178cc5b2deed817e8f1663d64a1df2ce548371ae
Alexis Roda <alexis.roda.villalonga@gmail.com>
2016-07-06 01:22:42 +0200

    Define base 'Factory' class.

---
+++ arv/factory/base.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# $Id: testing.py 4980 2016-07-04 19:13:06Z alex $
+
+# marker utilitzar per les factories per indicar que s'omet un atribut
+
+import inspect
+
+
+DELETE = object()
+
+
+# TODO: comprovar si un valor és un generador no és bona idea. Es pot
+# simular un generador amb un objecte. Cal mirar com generalitzar-ho.
+
+# TODO: en algunes factories de hera cal assignar valors a les
+# foreignkeys. Una possibilitat es redefinir el mètode 'dict' de la
+# factoria + super.
+
+
+class Factory(object):
+    """Class for defining factories.
+
+    Instances of ``Factory`` classes are actual factories. Calling an
+    instance creates a new object.
+
+    """
+
+    defaults = {}
+
+    def __init__(self, **kwargs):
+        d = self._get_defaults()
+        d.update(kwargs)
+        self._defaults = self._process_defaults(d)
+
+    def __call__(self, **kwargs):
+        res = {}
+        for k, v in self._defaults.items():
+            if k not in kwargs:
+                if inspect.isgenerator(v):
+                    res[k] = v.next()
+                else:
+                    res[k] = v
+        for k, v in kwargs.items():
+            if v is DELETE:
+                if k in res:
+                    del res[k]
+            else:
+                res[k] = v
+        return res
+
+    @classmethod
+    def _get_defaults(cls):
+        return dict.copy(cls.defaults)
+
+    def _process_defaults(self, d):
+        res = {}
+        for k, v in d.items():
+            if inspect.isgeneratorfunction(v):
+                res[k] = v()
+            elif v is not DELETE:
+                res[k] = v
+        return res
+
+
+def make_factory(defaults):
+    class F(Factory):
+        defaults = defaults
+    return F
---
+++ arv/factory/tests/test_factory.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+# $Id:$
+
+
+import inspect
+from unittest import TestCase
+
+from ..base import DELETE, Factory
+
+
+class TestProcessDefaults(TestCase):
+
+    def test_honors_DELETE(self):
+        factory = Factory()
+        d = {"foo": 1, "bar": DELETE}
+        self.assertEqual(
+            factory._process_defaults(d),
+            {"foo": 1}
+        )
+
+    def test_calls_generator_functions(self):
+        def generator():
+            yield 1
+        factory = Factory()
+        d = {"foo": 1, "bar": generator}
+        self.assertTrue(inspect.isgeneratorfunction(d["bar"]))
+        res = factory._process_defaults(d)
+        self.assertTrue(inspect.isgenerator(res["bar"]))
+
+
+class TestConstructor(TestCase):
+
+    def setUp(self):
+        class MyFactory(Factory):
+            defaults = {"foo": 1}
+        self.MyFactory = MyFactory
+
+    def test_kwargs_override_defaults(self):
+        factory = self.MyFactory(foo=2)
+        self.assertEqual(factory._defaults["foo"], 2)
+
+    def test_kwargs_honors_DELETE(self):
+        factory = self.MyFactory(foo=DELETE)
+        self.assertNotIn("foo", factory._defaults)
+
+
+class TestDict(TestCase):
+
+    def setUp(self):
+        def generator():
+            i = 1
+            while True:
+                yield i
+                i = i + 1
+
+        class MyFactory(Factory):
+            defaults = {
+                "foo": 1,
+                "bar": generator,
+            }
+        self.factory = MyFactory()
+
+    def test_scalar_value(self):
+        d = self.factory()
+        self.assertEqual(d["foo"], 1)
+
+    def test_generator_value(self):
+        d = self.factory()
+        self.assertEqual(d["bar"], 1)
+        d = self.factory()
+        self.assertEqual(d["bar"], 2)
+
+    def test_honors_DELETE(self):
+        d = self.factory(bar=DELETE)
+        self.assertNotIn("bar", d)
+
+    def test_kwargs_override_defaults(self):
+        d = self.factory(foo=3)
+        self.assertEqual(d["foo"], 3)
+
+    def test_kwarg_overriding_generator_dont_comsume_generator(self):
+        d = self.factory(bar=42)
+        self.assertEqual(d["bar"], 42)
+        d = self.factory()
+        self.assertEqual(d["bar"], 1)
