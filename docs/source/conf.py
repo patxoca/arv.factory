6dc28a78ea2cfea26b16f3560bcd370822739dc2
Alexis Roda <alexis.roda.villalonga@gmail.com>
2016-07-06 21:03:41 +0200

    Disable 'sphinx.ext.githubpages' extension.
    Building the docs in readthedocs complains about that extension is
    missing.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'sphinx.ext.githubpages',
+    # 'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
