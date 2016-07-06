71e800699150bcb66718c1febbf7998ba72fe33f
Alexis Roda <alexis.roda.villalonga@gmail.com>
2016-07-06 20:55:27 +0200

    Add 'DELETE' and 'escape' to the api.

--- arv/factory/api.py
+++ arv/factory/api.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from .base import DELETE        # noqa: F401
+from .base import escape        # noqa: F401
 from .base import Factory       # noqa: F401
 from .base import make_factory  # noqa: F401
