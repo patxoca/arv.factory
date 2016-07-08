4a60ad67a3b0ab18ffe738af4c71c88fff94f386
Alexis Roda <alexis.roda.villalonga@gmail.com>
2016-07-08 20:04:25 +0200

    Update tests.

--- arv/factory/tests/test_factory.py
+++ arv/factory/tests/test_factory.py
@@ -1,54 +1,50 @@
 # -*- coding: utf-8 -*-
 
-import inspect
-import itertools
 from unittest import TestCase
 
-from ..base import DELETE, Factory, escape
+from ..base import DELETE, Factory
+from ..generators import Gen
 
 
-class TestProcessDefaults(TestCase):
+class TestProcessMetafactoryArguments(TestCase):
 
     def test_honors_DELETE(self):
         factory = Factory()
         d = {"foo": 1, "bar": DELETE}
         self.assertEqual(
-            factory._process_defaults(d),
+            factory._process_metafactory_arguments(d),
             {"foo": 1}
         )
 
-    def test_calls_generator_functions(self):
-        def generator():
-            yield 1
+    def test_Gen_instances_are_not_evaluated(self):
         factory = Factory()
-        d = {"foo": 1, "bar": generator}
-        self.assertTrue(inspect.isgeneratorfunction(d["bar"]))
-        res = factory._process_defaults(d)
-        self.assertTrue(inspect.isgenerator(res["bar"]))
+        d = {"foo": 1, "bar": Gen([1])}
+        res = factory._process_metafactory_arguments(d)
+        self.assertTrue(isinstance(res["bar"], Gen))
 
-    def test_calls_functions(self):
-        def function():
-            return 42
+    def test_makes_value_generators_from_metafactories(self):
         factory = Factory()
-        d = {"foo": 1, "bar": function}
-        res = factory._process_defaults(d)
-        self.assertEqual(res["bar"], 42)
-
-    def test_escape_callable(self):
-        def function():
-            return 42
-        factory = Factory()
-        d = {"foo": 1, "bar": escape(function)}
-        res = factory._process_defaults(d)
-        self.assertIs(res["bar"], function)
+        d = {"foo": 1, "bar": Factory}
+        res = factory._process_metafactory_arguments(d)
+        self.assertIsInstance(res["bar"], Gen)
 
 
-class TestConstructor(TestCase):
+class TestFactoryConstructor(TestCase):
 
     def setUp(self):
         class MyFactory(Factory):
-            defaults = {"foo": 1}
+            defaults = {"foo": 1, "bar": Gen([1, 2, 3])}
         self.MyFactory = MyFactory
+
+    def test_collected_defaults(self):
+        factory = self.MyFactory()
+        self.assertEqual(len(factory._defaults), 2)
+        self.assertIn("foo", factory._defaults)
+        self.assertIn("bar", factory._defaults)
+
+    def test_Gen_instances_are_not_evaluated(self):
+        factory = self.MyFactory()
+        self.assertIsInstance(factory._defaults["bar"], Gen)
 
     def test_kwargs_override_defaults(self):
         factory = self.MyFactory(foo=2)
@@ -59,27 +55,16 @@ class TestConstructor(TestCase):
         self.assertNotIn("foo", factory._defaults)
 
 
-class TestDict(TestCase):
+class TestObjectCreation(TestCase):
 
     def setUp(self):
-        def generator():
-            i = 1
-            while True:
-                yield i
-                i = i + 1
+        self.factory = Factory(foo=1, bar=Gen([1, 2, 3, 4]))
 
-        class MyFactory(Factory):
-            defaults = {
-                "foo": 1,
-                "bar": generator,
-            }
-        self.factory = MyFactory()
-
-    def test_scalar_value(self):
+    def test_literal_value(self):
         d = self.factory()
         self.assertEqual(d["foo"], 1)
 
-    def test_generator_value(self):
+    def test_generators_are_consumed(self):
         d = self.factory()
         self.assertEqual(d["bar"], 1)
         d = self.factory()
@@ -88,6 +73,11 @@ class TestDict(TestCase):
     def test_honors_DELETE(self):
         d = self.factory(bar=DELETE)
         self.assertNotIn("bar", d)
+
+    def test_DELETE_ignores_missing_attributes(self):
+        self.assertNotIn("baz", self.factory._defaults)
+        d = self.factory(baz=DELETE)
+        self.assertNotIn("baz", d)
 
     def test_kwargs_override_defaults(self):
         d = self.factory(foo=3)
@@ -103,28 +93,19 @@ class TestDict(TestCase):
 class TestMany(TestCase):
 
     def setUp(self):
-        def generator():
-            i = 1
-            while True:
-                yield i
-                i = i + 1
-
-        class MyFactory(Factory):
-            defaults = {
-                "foo": 1,
-                "bar": generator,
-            }
-        self.factory = MyFactory()
+        self.factory = Factory(foo=1, bar=Gen([1, 2, 3, 4]))
 
     def test_negative_count_returns_empty_list(self):
         self.assertEqual(self.factory.many(-1), [])
 
     def test_number_of_objects(self):
-        num_obj = 10
+        num_obj = 3
         res = self.factory.many(num_obj)
         self.assertEqual(len(res), num_obj)
-        for i in range(num_obj):
-            d = res[i]
+
+    def test_objects_generated(self):
+        res = self.factory.many(3)
+        for i, d in enumerate(res):
             self.assertTrue(isinstance(d, dict))
             self.assertIn("foo", d)
             self.assertIn("bar", d)
@@ -132,21 +113,18 @@ class TestMany(TestCase):
             self.assertEqual(d["bar"], i + 1)
 
     def test_consumes_generators_in_kwargs(self):
-        num_obj = 10
-        res = self.factory.many(num_obj, foo=(i * i for i in itertools.count()))
-        for i in range(num_obj):
-            d = res[i]
+        res = self.factory.many(3, foo=Gen([0, 1, 4, 9]))
+        for i, d in enumerate(res):
             self.assertEqual(d["foo"], i * i)
             self.assertEqual(d["bar"], i + 1)
 
     def test_kwarg_overriding_generator_dont_consume_generator(self):
-        num_obj = 1
-        res = self.factory.many(num_obj, bar=(i * i for i in itertools.count()))
+        self.factory.many(2, bar=42)
         obj = self.factory()
         self.assertEqual(obj["bar"], 1)
 
 
-class TestAlternateConstructor(TestCase):
+class TestSpecifyingAlternateObjectConstructor(TestCase):
 
     def setUp(self):
         class Class(object):
@@ -155,6 +133,7 @@ class TestAlternateConstructor(TestCase):
 
         class MyFactory(Factory):
             constructor = Class
+
         self.Class = Class
         self.MyFactory = MyFactory
         self.factory = MyFactory()
