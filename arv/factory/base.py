6f1985ebe9bbdac604dc51f09a1bdd9c78f0f22d
Alexis Roda <alexis.roda.villalonga@gmail.com>
2016-07-08 20:01:04 +0200

    Remove magic from function and generators.
    Treating generators and functions as special values is minimalistic but
    makes the usage of the library a bit complex.

--- arv/factory/base.py
+++ arv/factory/base.py
@@ -2,23 +2,11 @@
 
 # marker utilitzar per les factories per indicar que s'omet un atribut
 
-import inspect
+from .generators import Gen
+from .generators import mkgen
 
 
 DELETE = object()
-
-
-# TODO: comprovar si un valor és un generador no és bona idea. Es pot
-# simular un generador amb un objecte. Cal mirar com generalitzar-ho.
-
-# TODO: en algunes factories de hera cal assignar valors a les
-# foreignkeys. Una possibilitat es redefinir el mètode 'dict' de la
-# factoria + super.
-
-def escape(obj):
-    def wrapper():
-        return obj
-    return wrapper
 
 
 class Factory(object):
@@ -35,10 +23,10 @@ class Factory(object):
     def __init__(self, **kwargs):
         d = self._get_defaults()
         d.update(kwargs)
-        self._defaults = self._process_defaults(d)
+        self._defaults = self._process_metafactory_arguments(d)
 
     def __call__(self, **kwargs):
-        res = self._expand_dict(self._defaults, exclude=set(kwargs.keys()))
+        res = self._eval_arguments(self._defaults, exclude=set(kwargs.keys()))
         for k, v in kwargs.items():
             if v is DELETE:
                 if k in res:
@@ -51,7 +39,7 @@ class Factory(object):
         res = []
         while count > 0:
             count = count - 1
-            d = self._expand_dict(kwargs)
+            d = self._eval_arguments(kwargs)
             res.append(self(**d))
         return res
 
@@ -59,20 +47,20 @@ class Factory(object):
     def _get_defaults(cls):
         return dict.copy(cls.defaults)
 
-    def _process_defaults(self, d):
+    def _process_metafactory_arguments(self, d):
         res = {}
         for k, v in d.items():
-            if callable(v):
-                res[k] = v()
+            if isinstance(v, type) and issubclass(v, Factory):
+                res[k] = mkgen(v())
             elif v is not DELETE:
                 res[k] = v
         return res
 
-    def _expand_dict(self, d, exclude=()):
+    def _eval_arguments(self, d, exclude=()):
         res = {}
         for k, v in d.items():
             if k not in exclude:
-                if inspect.isgenerator(v):
+                if isinstance(v, Gen):
                     res[k] = v.next()
                 else:
                     res[k] = v
