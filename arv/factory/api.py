3865483a971b8fd15505a5c5413da19d7d4e3315
Alexis Roda <alexis.roda.villalonga@gmail.com>
2016-07-12 20:11:03 +0200

    Update publica API.

--- arv/factory/api.py
+++ arv/factory/api.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from . import generators as gen  # noqa: F401
 from .base import DELETE        # noqa: F401
-from .base import escape        # noqa: F401
 from .base import Factory       # noqa: F401
 from .base import make_factory  # noqa: F401
+from .generators import Gen     # noqa: F401
+from .generators import lazy    # noqa: F401
+from .generators import mkconstructor   # noqa: F401
+from .generators import mkgen   # noqa: F401
