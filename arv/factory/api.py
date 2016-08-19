9d3471a562c8c38a1e5dd2998adac3d2a4124f18
Alexis Roda <alexis.roda.villalonga@gmail.com>
2016-08-19 22:39:59 +0200

    Remove funtion from API.
    Removed from 'base.py' in commit e502175 but not from 'api.py'.

--- arv/factory/api.py
+++ arv/factory/api.py
@@ -4,7 +4,6 @@ from . import generators as gen  # noqa: F401
 from .base import DjangoFactory  # noqa: F401
 from .base import DELETE        # noqa: F401
 from .base import Factory       # noqa: F401
-from .base import make_factory  # noqa: F401
 from .generators import Gen     # noqa: F401
 from .generators import lazy    # noqa: F401
 from .generators import mkconstructor   # noqa: F401
